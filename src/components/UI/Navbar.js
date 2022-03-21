b3438645a7cb5264ca886c3c082c74237fb89cdd
Ricardo Juárez Tepos <45005831+RichieSjt@users.noreply.github.com>
2022-03-21 12:58:37 -0600

    Navbar active classname

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { header, navbar, navlinks, menuBtn } from '../../styles/UI/navbar.module.scss'
+import { header, navbar, navlinks, active, menuBtn } from '../../styles/UI/navbar.module.scss'
 
 import MenuBtn from '../../icons/menu-icon.svg'
 
@@ -7,22 +7,49 @@ import { Link } from 'gatsby'
 import Button from './Button'
 
 const Navbar = () => {
+    // Setting active state based on location hash since gatsby does not match /#something paths by default 
+    const isActive = ({ href, location }) => {
+        // location.hash is the hash in the link (#something), if it is empy we set it
+        // to about so that it applies that link the active class by default
+        if (location.hash === '') location.hash = '#about'
+        return href.includes(location.hash) ? { className: active } : {}
+    }
+
     return (
         <header className={header}>
             <nav className={navbar}>
                 <input type="checkbox" id="menu-btn" />
-                <label htmlFor="menu-btn" class={menuBtn}>
+                <label htmlFor="menu-btn" className={menuBtn}>
                     <MenuBtn />
                 </label>
                 <ul className={navlinks}>
                     <li>
-                        <Link to="/#about">About</Link>
+                        <Link
+                            to="/#about"
+                            // Get props passes the specified function an object containing link properties
+                            getProps={isActive}
+                            activeClassName={active}
+                        >
+                            About
+                        </Link>
                     </li>
                     <li>
-                        <Link to="/#projects">Projects</Link>
+                        <Link
+                            to="/#projects"
+                            getProps={isActive}
+                            activeClassName={active}
+                        >
+                            Projects
+                        </Link>
                     </li>
                     <li>
-                        <Link to="/#contact">Contact</Link>
+                        <Link
+                            to="/#contact"
+                            getProps={isActive}
+                            activeClassName={active}
+                        >
+                            Contact
+                        </Link>
                     </li>
                 </ul>
                 <Button link="/resume.pdf" newTab={true}>
